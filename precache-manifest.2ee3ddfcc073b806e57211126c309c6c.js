ae06b9df3860fc28a138492a9eecce0e7f546d3d
Arvydas <arvydasbliujus@gmail.com>
2020-09-21 12:41:46 +0300

    Updates

---
+++ precache-manifest.2ee3ddfcc073b806e57211126c309c6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "197f62285e60f5bce5ad820bdce98dfb",
+    "url": "/currency-react-app/index.html"
+  },
+  {
+    "revision": "0356cb3e5d91b4501996",
+    "url": "/currency-react-app/static/css/main.b2e210a3.chunk.css"
+  },
+  {
+    "revision": "102b15d6804e3c1959de",
+    "url": "/currency-react-app/static/js/2.c80eae17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-react-app/static/js/2.c80eae17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0356cb3e5d91b4501996",
+    "url": "/currency-react-app/static/js/main.190d8373.chunk.js"
+  },
+  {
+    "revision": "74b8ea50c6576f0797e9",
+    "url": "/currency-react-app/static/js/runtime-main.69577f40.js"
+  }
+]);
